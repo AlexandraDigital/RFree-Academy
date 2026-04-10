e9607b443d4556f3cfc61d50d9b1057fdf559848
AlexandraDigital <futuresuccess105@gmail.com>
2026-04-09 22:10:57 -0400

    Delete functions/add-course.js

--- functions/add-course.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(req, res) {
-  const token = req.headers.authorization;
-  if (!token) return res.status(403).json({ error: 'Forbidden' });
-  return res.status(200).json({ message: 'Course added successfully' });
-}
